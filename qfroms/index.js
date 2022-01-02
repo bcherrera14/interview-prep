5ad0ad9a98907d369b1c8cd8eb773b3444de301c
Bryan Herrera <bcherrera14@gmail.com>
2022-01-01 16:08:00 -0800

    add qforms solution

--- qfroms/index.js
+++ qfroms/index.js
@@ -12,8 +12,53 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
+//queue is first in first out
+//adds to beginning of array
+//removes last item in array
+//peek shows last item in array
+
+//stack is first in last out
+//push adds to end of array
+//pop removes last item in array
+//peek returns last item in array
+
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+	constructor() {
+		this.stackOne = new Stack();
+		this.stackTwo = new Stack();
+	}
+
+	add(record) {
+		this.stackOne.push(record);
+	}
+
+	remove() {
+		while (this.stackOne.peek()) {
+			this.stackTwo.push(this.stackOne.pop());
+		}
+
+		let record = this.stackTwo.pop();
+
+		while (this.stackTwo.peek()) {
+			this.stackOne.push(this.stackTwo.pop());
+		}
+		return record;
+	}
+
+	peek() {
+		while (this.stackOne.peek()) {
+			this.stackTwo.push(this.stackOne.pop());
+		}
+
+		let record = this.stackTwo.peek();
+
+		while (this.stackTwo.peek()) {
+			this.stackOne.push(this.stackTwo.pop());
+		}
+		return record;
+	}
+}
 
 module.exports = Queue;
